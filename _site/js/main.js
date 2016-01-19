e5c447a71c0944ccef1cfad0a9cded4115aa46ba
Robert Bayer <robertwbayer@gmail.com>
2016-01-18 16:52:44 -0800

    personal site updates

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
-  $(".data").gridalicious({
-    width: 300,
-    gutter: 10,
-    animate: false
-  });
-});
+	var userID = "13852165";
+	var accessToken = "13852165.1677ed0.43566eacef4b419dba014bb9c6af3318";
+	var biggestWidth = 213;
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+	setDivPosition(windowWidth, windowHeight);
+
+	immixr(8, .4, userID, accessToken, biggestWidth);
+
+	function setDivPosition(windowWidth, windowHeight) {
+
+	  var elementWidth = $("div#info").width();
+	  var elementHeight = $("div#info").height();
+
+	  var leftPosition = (windowWidth - elementWidth) / 2;
+	  var topPosition = (windowHeight - elementHeight) / 2;
+
+	  $("#info").css({top: topPosition, left: leftPosition});
+
+	}
+});
